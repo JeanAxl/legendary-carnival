bd0905d1a668384ff6edef6c017d795640b68753
Axel <axelbie64@gmail.com>
2020-01-12 10:50:58 +0100

    Starts app bar

--- src/components/app-bar/AppBar.tsx
+++ src/components/app-bar/AppBar.tsx
@@ -3,18 +3,28 @@ import makeStyles from "@material-ui/core/styles/makeStyles";
 import { createStyles, Theme } from "@material-ui/core";
 
 import MUIAppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
+import { grey } from "@material-ui/core/colors";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      flexGrow: 1,
+      background: grey[500],
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
     },
-    menuButton: {
-      marginRight: theme.spacing(2),
+    link: {
+      textTransform: "uppercase",
+      textAlign: "center",
+      width: "5%",
+      margin: theme.spacing(2),
+    },
+    logo: {
+      margin: theme.spacing(2),
+      width: "5%",
+      fontSize: "3rem",
+      textAlign: "center",
+      fontWeight: "bold",
     },
   }),
 );
@@ -24,14 +34,13 @@ export const AppBar: React.FC = () => {
 
   return (
     <MUIAppBar position="sticky">
-      <Toolbar variant="dense">
-        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" color="inherit">
-          Photos
-        </Typography>
-      </Toolbar>
+      <div className={classes.root}>
+        <div className={classes.link}>About</div>
+        <div className={classes.link}>Join</div>
+        <div className={classes.logo}>AG</div>
+        <div className={classes.link}>devis</div>
+        <div className={classes.link}>contact</div>
+      </div>
     </MUIAppBar>
   );
 };
